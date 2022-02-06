8333613fecd6b0cb24e69c1c4cfbba2ab44748ff
Derrick Yoo <4998684+derrickyoo@users.noreply.github.comderrickyoo@gmail.com>
2022-02-05 18:45:05 -0800

    Update eslint for react

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return React.createElement("h1", {}, "React Poke");
+  return (
+    <div>
+      <h1>React Poke</h1>
+    </div>
+  );
 };
 
 const container = document.getElementById("root");
