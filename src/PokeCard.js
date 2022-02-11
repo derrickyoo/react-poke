970fd508fea8d966b32781de3a71fb9a9c1b9992
Derrick Yoo <4998684+derrickyoo@users.noreply.github.comderrickyoo@gmail.com>
2022-02-11 10:52:34 -0800

    Add global styles and poke card

--- src/PokeCard.js
+++ src/PokeCard.js
@@ -4,17 +4,45 @@ const PokeCard = ({ pokemon }) => {
   return (
     <Wrapper>
       <Avatar src={pokemon.ThumbnailImage} alt={pokemon.ThumbnailAltText} />
-      <Name>{pokemon.name}</Name>
       <Number>{pokemon.number}</Number>
+      <Name>{pokemon.name}</Name>
       <Type>{pokemon.type.join(" ")}</Type>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.article``;
-const Avatar = styled.img``;
-const Name = styled.h2``;
-const Number = styled.p``;
+const Wrapper = styled.article`
+  min-width: 250px;
+  border-radius: 24px;
+  padding: 24px;
+  background: var(--white);
+  box-shadow: var(--shadow-elevation-medium);
+  text-align: center;
+`;
+
+const Avatar = styled.img`
+  background: var(--white);
+  width: 128px;
+  height: 128px;
+  border: 1px solid var(--black);
+  border-radius: 50%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 16px;
+  margin-top: -64px;
+`;
+
+const Name = styled.h2`
+  margin-bottom: 8px;
+`;
+
+const Number = styled.p`
+  margin-bottom: 8px;
+  &::before {
+    content: "#";
+  }
+`;
+
 const Type = styled.p``;
 
 export default PokeCard;
