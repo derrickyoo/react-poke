edecf0991d63dad7c50f16ca5cc994ba690287a3
Derrick Yoo <4998684+derrickyoo@users.noreply.github.comderrickyoo@gmail.com>
2022-02-10 17:22:58 -0800

    Clean up App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
-import React, { Fragment } from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom";
 import GlobalStyles from "./GlobalStyles";
 import PokeLayout from "./PokeLayout";
 
 const App = () => {
   return (
-    <Fragment>
+    <>
       <PokeLayout />
       <GlobalStyles />
-    </Fragment>
+    </>
   );
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+const root = createRoot(container);
+root.render(<App />);
