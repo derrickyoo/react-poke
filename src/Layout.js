54cf84e21087619307c2bca5520a82d0df0e8735
Derrick Yoo <4998684+derrickyoo@users.noreply.github.comderrickyoo@gmail.com>
2022-02-11 16:22:41 -0800

    Add seed data for demo

--- src/Layout.js
+++ src/Layout.js
@@ -1,66 +1,24 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components/macro";
 import PokeSearch from "./PokeSearch";
 import PokeDeck from "./PokeDeck";
-
-const POKE_DATA = [
-  {
-    abilities: ["Overgrow"],
-    weight: 15.2,
-    weakness: ["Fire", "Psychic", "Flying", "Ice"],
-    number: "001",
-    height: 28.0,
-    collectibles_slug: "bulbasaur",
-    featured: "true",
-    slug: "bulbasaur",
-    name: "Bulbasaur",
-    ThumbnailAltText: "Bulbasaur",
-    ThumbnailImage:
-      "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png",
-    id: 1,
-    type: ["grass", "poison"],
-  },
-  {
-    abilities: ["Overgrow"],
-    weight: 28.7,
-    weakness: ["Fire", "Psychic", "Flying", "Ice"],
-    number: "002",
-    height: 39.0,
-    collectibles_slug: "ivysaur",
-    featured: "true",
-    slug: "ivysaur",
-    name: "Ivysaur",
-    ThumbnailAltText: "Ivysaur",
-    ThumbnailImage:
-      "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/002.png",
-    id: 2,
-    type: ["grass", "poison"],
-  },
-  {
-    abilities: ["Overgrow"],
-    weight: 9999.0,
-    weakness: ["Fire", "Psychic", "Flying", "Ice"],
-    number: "003",
-    height: 945.0,
-    collectibles_slug: "venusaur",
-    featured: "true",
-    slug: "venusaur",
-    name: "Venusaur",
-    ThumbnailAltText: "Venusaur",
-    ThumbnailImage:
-      "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png",
-    id: 3,
-    type: ["grass", "poison"],
-  },
-];
+import data from "./data.json";
 
 const Layout = () => {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setPokemon(data), 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <Wrapper>
       {/* Section: PokeSearch */}
       <PokeSearch />
 
       {/* Section: PokeDeck */}
-      <PokeDeck pokemon={POKE_DATA}></PokeDeck>
+      <PokeDeck pokemon={pokemon}></PokeDeck>
     </Wrapper>
   );
 };
